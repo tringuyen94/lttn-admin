a83bdfb111d95f20ef88b14441444d92c64ae96d
mikey299 <tringuyen194.dev@icloud.com>
2022-01-15 11:21:44 +0700

    commit build files

---
+++ lttn-admin/precache-manifest.6548970e953edf56d49a0b9a4238fd3f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb491243431930d752ed19f7628d812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "054fa029a4a18cefadf2",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "2e921a6618df26ddeecb",
+    "url": "/static/css/main.b469df37.chunk.css"
+  },
+  {
+    "revision": "054fa029a4a18cefadf2",
+    "url": "/static/js/2.c2e3da86.chunk.js"
+  },
+  {
+    "revision": "9365dc5c1e8ab62f1f77fb80200ba2f4",
+    "url": "/static/js/2.c2e3da86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e921a6618df26ddeecb",
+    "url": "/static/js/main.7f46af72.chunk.js"
+  },
+  {
+    "revision": "50ac3dc11cf412bd099a",
+    "url": "/static/js/runtime-main.afb6abcb.js"
+  },
+  {
+    "revision": "f96e992c82416c04f0b3a3a415bb6a80",
+    "url": "/static/media/brand.f96e992c.jpg"
+  },
+  {
+    "revision": "5b7f3c2abb557ff9509aeb86bfe29da6",
+    "url": "/static/media/category.5b7f3c2a.png"
+  },
+  {
+    "revision": "66c68fd821ceef385bcfdc1f2b306014",
+    "url": "/static/media/logoLTTN.66c68fd8.jpg"
+  },
+  {
+    "revision": "f46aa65e1b82be978cf7cdb76d66c5fc",
+    "url": "/static/media/main-background.f46aa65e.jpg"
+  },
+  {
+    "revision": "8b7b84e083683a1a4e128b907a301169",
+    "url": "/static/media/product.8b7b84e0.jpg"
+  }
+]);
