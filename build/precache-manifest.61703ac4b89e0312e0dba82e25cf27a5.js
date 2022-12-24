65c18cfeddd270da1033b17437a6f21d99a6db61
mikey <mikey@git.com>
2022-12-24 20:23:33 +0700

    build folder

---
+++ build/precache-manifest.61703ac4b89e0312e0dba82e25cf27a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b724464b7531466b42ad66f43fd8c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2890751f7af61583d9b",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "4ee42218b0ef2a4580e6",
+    "url": "/static/css/main.7dd4453f.chunk.css"
+  },
+  {
+    "revision": "b2890751f7af61583d9b",
+    "url": "/static/js/2.5cbd3f36.chunk.js"
+  },
+  {
+    "revision": "9365dc5c1e8ab62f1f77fb80200ba2f4",
+    "url": "/static/js/2.5cbd3f36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee42218b0ef2a4580e6",
+    "url": "/static/js/main.2b5de78d.chunk.js"
+  },
+  {
+    "revision": "50ac3dc11cf412bd099a",
+    "url": "/static/js/runtime-main.afb6abcb.js"
+  },
+  {
+    "revision": "66c68fd821ceef385bcfdc1f2b306014",
+    "url": "/static/media/logoLTTN.66c68fd8.jpg"
+  },
+  {
+    "revision": "f46aa65e1b82be978cf7cdb76d66c5fc",
+    "url": "/static/media/main-background.f46aa65e.jpg"
+  }
+]);
